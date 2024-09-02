e84f05e806790f3b6cb8b230417030bd6bd14b31
Vivek Singh <singhvivek357480@gmail.com>
2024-09-03 02:54:34 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-2xl bg-gray-500 '>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
